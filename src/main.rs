cc9853312df1d58af9672427f9788f9639f4abaa
Rob Pickerill <rob.pickerill@disneystreaming.com>
2022-11-22 12:57:08 +0000

    add clap for cli args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::{Arc, Mutex};
 
+use clap::Parser;
 use tokio::sync::Semaphore;
 
 mod cloudwatch;
@@ -7,22 +8,37 @@ mod quota;
 mod service;
 mod util;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// the threshold by which to alert on for utilization of a service quota
+    #[arg(short, long, default_value_t = 75)]
+    threshold: u8,
+}
+
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+    run(args).await;
+}
+
+async fn run(args: Args) {
     let services = service::list_service_codes().await;
 
     let mut handlers = Vec::with_capacity(services.len());
-    let permits = Arc::new(Semaphore::new(10));
+    let permits = Arc::new(Semaphore::new(5));
 
-    let mut breached_quotas = Arc::new(Mutex::new(Vec::new()));
+    let breached_quotas = Arc::new(Mutex::new(Vec::new()));
+
     for service in services {
         let permits = Arc::clone(&permits);
         let breached_quotas = Arc::clone(&breached_quotas);
+
         let handler = tokio::spawn(async move {
             let _permit = permits.acquire().await.unwrap();
             let _breached_quotas = breached_quotas.clone();
 
-            let client = service::Client::new(75).await;
+            let client = service::Client::new(args.threshold).await;
             let quotas = client.breached_quotas(&service).await;
 
             match quotas {
@@ -48,22 +64,4 @@ async fn main() {
     for quota in breached_quotas.lock().unwrap().iter() {
         println!("{:?}", quota);
     }
-    // let handle1 = tokio::spawn(async move {
-    //     let client = service::Client::new().await;
-    //     println!("{:?}", client.quotas("sagemaker").await);
-    // });
-
-    // let handle2 = tokio::spawn(async move {
-    //     let client = service::Client::new().await;
-    //     println!("{:?}", client.quotas("cloudformation").await);
-    // });
-
-    // let handle3 = tokio::spawn(async move {
-    //     let client = service::Client::new().await;
-    //     println!("{:?}", client.quotas("dynamodb").await);
-    // });
-
-    // handle1.await.unwrap();
-    // handle2.await.unwrap();
-    // handle3.await.unwrap();
 }
