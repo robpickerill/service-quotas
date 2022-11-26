d9929fe2d1aa6107a454684e3d1c56a38606c7f3
Rob Pickerill <rob.pickerill@disneystreaming.com>
2022-11-26 20:04:40 +0000

    add help

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,15 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// the threshold by which to alert on for utilization of a service quota
-    #[arg(short, long, default_value_t = 75)]
+    #[arg(
+        short,
+        long,
+        default_value_t = 75,
+        help = "the threshold to alert at for utlization of a service quota"
+    )]
     threshold: u8,
 
-    #[arg(short, long, num_args(0..))]
+    #[arg(short, long, num_args(0..), help="the AWS region(s) to check quotas for, defaults to all")]
     regions: Vec<String>,
 }
 #[tokio::main]
