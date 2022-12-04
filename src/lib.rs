0ba2225185dfc2246f4fb657b9a0c0d552f735f1
Rob Pickerill <rob.pickerill@disneystreaming.com>
2022-12-04 13:22:26 +0000

    add logging on breaches

--- src/lib.rs
+++ src/lib.rs
@@ -7,57 +7,13 @@ mod services;
 mod util;
 
 use async_mutex::Mutex;
+use quota::Quota;
 use services::servicequota;
 use std::{collections::HashSet, sync::Arc};
 use tokio::sync::Semaphore;
 
 #[macro_use]
 extern crate log;
-
-fn lift_pagerduty_routing_key() -> Option<String> {
-    std::env::var("PAGERDUTY_ROUTING_KEY").ok()
-}
-
-async fn notify(
-    notifier: impl notifiers::Notifier,
-    breached_quotas: Arc<Mutex<Vec<quota::Quota>>>,
-    ignored_quotas: &HashSet<String>,
-) {
-    for quota in breached_quotas.lock().await.iter() {
-        if ignored_quotas.contains(&quota.quota_code().to_string()) {
-            info!(
-                "Ignoring quota {} as it is in the ignore list",
-                quota.quota_code()
-            );
-            continue;
-        }
-
-        let result = notifier.notify(quota.clone()).await;
-
-        if let Err(err) = result {
-            println!("pagerduty error: {}", err)
-        }
-    }
-}
-
-fn log_startup(config: &config::Config) {
-    info!(
-        "Starting up: {} {}",
-        env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_VERSION")
-    );
-    info!("Region: {}", config.regions().join(", "));
-    info!("Threshold: {}", config.threshold());
-    info!(
-        "Ignored quotas: {}",
-        config
-            .ignored_quotas()
-            .iter()
-            .map(|s| s.to_string())
-            .collect::<Vec<_>>()
-            .join(", ")
-    );
-}
 
 pub async fn run(args: &clap::ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
     let config = config::Config::new(args);
@@ -112,18 +68,7 @@ pub async fn run(args: &clap::ArgMatches) -> Result<(), Box<dyn std::error::Erro
         }
     }
 
-    for quota in all_quotas.lock().await.iter() {
-        if quota.utilization() > Some(config.threshold()) {
-            info!(
-                "{:15}: {:30} {:12} {:30} : {:3}%",
-                quota.region(),
-                quota.service_code(),
-                quota.quota_code(),
-                quota.name(),
-                quota.utilization().unwrap()
-            )
-        }
-    }
+    log_breached_quotas(Arc::clone(&all_quotas), &config).await;
 
     if let Some(pd_key) = lift_pagerduty_routing_key() {
         let pagerduty = notifiers::pagerduty::Client::new(&pd_key, config.threshold())?;
@@ -131,4 +76,74 @@ pub async fn run(args: &clap::ArgMatches) -> Result<(), Box<dyn std::error::Erro
     }
 
     Ok(())
+}
+
+fn lift_pagerduty_routing_key() -> Option<String> {
+    std::env::var("PAGERDUTY_ROUTING_KEY").ok()
+}
+
+async fn notify(
+    notifier: impl notifiers::Notifier,
+    breached_quotas: Arc<Mutex<Vec<quota::Quota>>>,
+    ignored_quotas: &HashSet<String>,
+) {
+    for quota in breached_quotas.lock().await.iter() {
+        if ignored_quotas.contains(&quota.quota_code().to_string()) {
+            info!(
+                "Ignoring quota {} as it is in the ignore list",
+                quota.quota_code()
+            );
+            continue;
+        }
+
+        let result = notifier.notify(quota.clone()).await;
+
+        if let Err(err) = result {
+            println!("pagerduty error: {}", err)
+        }
+    }
+}
+
+fn log_startup(config: &config::Config) {
+    info!(
+        "Starting up: {} {}",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION")
+    );
+    info!("Region: {}", config.regions().join(", "));
+    info!("Threshold: {}", config.threshold());
+    info!(
+        "Ignored quotas: {}",
+        config
+            .ignored_quotas()
+            .iter()
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>()
+            .join(", ")
+    );
+}
+
+async fn log_breached_quotas(quotas: Arc<Mutex<Vec<Quota>>>, config: &config::Config) {
+    let mut count = 0;
+
+    for quota in quotas.lock().await.iter() {
+        if quota.utilization() > Some(config.threshold())
+            && !config.ignored_quotas().contains(quota.quota_code())
+        {
+            info!(
+                "{:15}: {:30} {:12} {:30} : {:3}%",
+                quota.region(),
+                quota.service_code(),
+                quota.quota_code(),
+                quota.name(),
+                quota.utilization().unwrap()
+            );
+
+            count += 1;
+        }
+    }
+
+    if count == 0 {
+        info!("No quotas breached");
+    }
 }
