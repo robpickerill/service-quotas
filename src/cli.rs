6d7d71da8780b9671a97ae03d85920ebcf7a0d5f
Rob Pickerill <rob.pickerill@disneystreaming.com>
2022-12-04 15:05:27 +0000

    remove unused args

--- src/cli.rs
+++ src/cli.rs
@@ -21,17 +21,6 @@ fn list_quotas() -> Command {
     Command::new("list-quotas")
         .about("List all quotas")
         .args(&common_args())
-        .args(vec![
-            Arg::new("threshold")
-                .short('t')
-                .long("threshold")
-                .help("The threshold to alert at for utlization of a service quota"),
-            Arg::new("ignore")
-                .short('i')
-                .long("ignore")
-                .num_args(1..)
-                .help("The quota codes to ignore, defaults to none"),
-        ])
 }
 
 fn utilization() -> Command {
